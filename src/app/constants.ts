05aababf063c8756f59b968f11409cb0c7aaa89a
Godspower Eze <Godspowereze260@gmail.com>
2024-10-01 06:49:10 +0300

    feat: update

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,7 +3,7 @@ import {
   MultilinearLagrangeInterpolationStepsAndEvaluations,
 } from "./utils/latex";
 
-export const BACKEND_URL = "http://127.0.0.1:8080/";
+export const BACKEND_URL = "https://api.modcc.xyz/";
 
 ////////////////////////////////////////////////////////
 ////////////// LAGRANGE INTERPOLATION /////////////////
